a373fc22cbba09c57b32445dd3d845d98b8abe28
eldarissimo <eishmuratov@yandex.ru>
2022-10-30 23:01:07 +0300

    MathCalculator

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double sumSubstrAndDivison(double first, double second) {
+        return substraction(first, second)
+                + division(first, second);
+    }
+
+    public static double allSum(double first, double second) {
+        return sum(first, second) + multiply(first, second) + substraction(first, second)
+                + division(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + sumSubstrAndDivison(5, 10));
+        System.out.println("Результат расчета равен: " + allSum(10, 20));
+
+    }
+}
